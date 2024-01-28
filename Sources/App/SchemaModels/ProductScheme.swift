2d0a80702ef1811479ca96f8796a9da5f53e4436
Proped <66710263+nattaponGolf123@users.noreply.github.com>
2024-01-29 05:48:46 +0700

    new productScema

--- Sources/App/SchemaModels/ProductScheme.swift
+++ Sources/App/SchemaModels/ProductScheme.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by IntrodexMac on 28/1/2567 BE.
 //
@@ -11,21 +11,34 @@ import Vapor
 
 final class ProductScheme: Model, Content {
     static let schema = "products"
-
+    
     @ID(key: .id)
     var id: UUID?
-
+    
     @Field(key: "name")
     var name: String
-
-    @Field(key: "email")
-    var email: String
-
+    
+    @Field(key: "price")
+    var price: Double
+    
+    @Field(key: "description")
+    var description: String
+    
+    @Field(key: "unit")
+    var unit: String
+    
     init() { }
-
-    init(id: UUID? = nil, name: String, email: String) {
+    
+    init(id: UUID? = nil,
+         name: String,
+         price: Double,
+         description: String,
+         unit: String) {
         self.id = id
         self.name = name
-        self.email = email
+        self.price = price
+        self.description = description
+        self.unit = unit
     }
+    
 }
